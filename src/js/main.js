6a8f426bfaa6e64527eb9d29a79abd7c97252ee2
brandonherrera2025-gif <brandonherrera2025@gmail.com>
2026-05-12 20:46:55 -0500

    coco

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,35 @@
 // --- TUS PRÁCTICAS AQUÍ ABAJO ---
 console.log("DOM Playground listo para practicar 🚀")
+// sumar
+const contador = document.querySelector("#counter-display");
+
+let numero = 0;
+
+const botonSuma = document.querySelector("#increment");
+ 
+botonSuma.addEventListener("click", () =>{
+    numero++;
+    contador.innerText = numero;
+});
+// restar
+const botonRestar = document.querySelector("#decrement");
+
+
+botonRestar.addEventListener("click", () =>{
+    numero--;
+    contador.innerText = numero;
+});
+ // resest 
+ const botonReset = document.querySelector("#reset");
+
+ botonReset.addEventListener("click", () =>{
+    numero = 0
+    contador.innerText = numero;
+ });
+// cambiar el color a rojo
+if (numero < 0){
+    contador.style.color = "red";
+}else{
+    contador.style.color = "black"
+}
+
